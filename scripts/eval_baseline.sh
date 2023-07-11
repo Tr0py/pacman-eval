30955749e2b5aa29a6e17ace7d234c6947a6e66e
ziyi <troppingz@gmail.com>
2023-07-11 02:13:41 -0500

    script: auto run etc for vpm.

---
+++ scripts/eval_baseline.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+NAME=$1
+CMD=$2
+LOG_DIR=$NAME
+
+if [ -z "$1" ]
+then
+	echo "Usage: $0 <task_name> <command-wrapped-with-\">"
+	exit 1
+fi
+
+mkdir -p $LOG_DIR
+
+echo "Got task name $NAME, command $CMD"
+
+LOG_FILE=$LOG_DIR/$NAME-baseline.log
+echo -n "Init done. Current time:"
+date
+echo "Executing commands, output written to logfile: $LOG_FILE"
+/usr/bin/time $CMD &> ./$LOG_FILE
---
+++ scripts/eval_vpm_etc.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Check if exactly two arguments are given
+if [ "$#" -ne 2 ]; then
+    echo "Error: You must enter exactly 2 arguments"
+    echo "This script requires a script name and a task name."
+    echo "Usage: $0 [script_name] [task_name]"
+    echo "Example: $0 ./eval_baseline.sh vpm-50-4-2-nopdf"
+    echo "Example: $0 ~/eval_with_resizing_pm.sh vpm-50-4-2-nopdf"
+    exit 1
+fi
+
+# Assigning arguments to variables
+script_name=$1
+task_name=$2
+
+$script_name  ${task_name}-flatstore-ph-etc "./eval_etc.sh 2 0"
+$script_name  ${task_name}-flatstore-ph-pacman-etc "./eval_etc.sh 2 1"
+$script_name  ${task_name}-flatstore-ff-etc "./eval_etc.sh 3 0"
+$script_name  ${task_name}-flatstore-ff-pacman-etc "./eval_etc.sh 3 1"
+$script_name  ${task_name}-chameleondb-etc "./eval_etc.sh 6 0"
+$script_name  ${task_name}-chameleondb-pacman-etc "./eval_etc.sh 6 1"
+$script_name  ${task_name}-pmemrocksdb-etc "./eval_etc.sh 7 0"
+
